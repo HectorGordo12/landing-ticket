f2cf9f19f8b597e615fce406520790a284c0735e
Виктор <viktor@MacBook-Air-Viktor.local>
2024-10-24 02:45:55 +0200

    Update partners page

--- src/pages/Partners.jsx
+++ src/pages/Partners.jsx
@@ -34,11 +34,51 @@ const Partners = () => {
           </figcaption>
         </figure>
 
-        <figure className={styles.figure}>
+        <figure className={`${styles.figure} ${styles.swap}`}>
           <img
             src="https://images.squarespace-cdn.com/content/v1/5e8e02eb5040c0487c716fa7/30e1e029-319b-4cb6-8253-e05952474d1e/E_TOD3948+copy.JPG?format=2500w"
             alt="LOS ANGELES RAMS"
-            className={styles.imageWide}
+            className={styles.image}
+          />
+          <figcaption className={styles.caption}>
+            <h2 className={styles.name}>LOS ANGELES RAMS</h2>
+            <p className={styles.description}>
+              As the Official Entertainment Concierge, Confirmed360 will provide
+              exclusive customizable services, access, and experiences to the
+              most coveted live entertainment events to the team and its fans.
+            </p>
+            <a href="https://www.raidersexperiences.com/">
+              <p className={styles.more}>LEARN MORE</p>
+            </a>
+          </figcaption>
+        </figure>
+        <figure className={styles.figure}>
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/5e8e02eb5040c0487c716fa7/fb273854-a4f6-4b69-b34d-c2e2e2051972/863EFFD5-32A2-49F1-8051-6F26203EDD4E.JPG?format=2500w"
+            alt="Las Vegas Raiders"
+            className={styles.image}
+          />
+          <figcaption className={styles.caption}>
+            <h2 className={styles.name}>LAS VEGAS RAIDERS</h2>
+            <p className={styles.description}>
+              Confirmed360 is the Official Experience Provider of the Las Vegas
+              Raiders. Through this partnership we produce the ultimate gameday
+              experience, starting with the Raiders Pregame MASQUERAID! This VIP
+              tailgate includes a 45 foot pirate ship with a DJ booth, live
+              performances, all inclusive F&B, open bars, lounges, TVs, misting
+              areas and much more!
+            </p>
+            <a href="https://www.raidersexperiences.com/">
+              <p className={styles.more}>LEARN MORE</p>
+            </a>
+          </figcaption>
+        </figure>
+
+        <figure className={`${styles.figure} ${styles.swap}`}>
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/5e8e02eb5040c0487c716fa7/30e1e029-319b-4cb6-8253-e05952474d1e/E_TOD3948+copy.JPG?format=2500w"
+            alt="LOS ANGELES RAMS"
+            className={styles.image}
           />
           <figcaption className={styles.caption}>
             <h2 className={styles.name}>LOS ANGELES RAMS</h2>
